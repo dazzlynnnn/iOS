4073ffb0f6839756eaaff19324d545d3c6a3d4bb
Lynn <dazzlynnnn@gmail.com>
2021-01-05 13:47:25 +0900

    Initial commit

---
+++ Ch2_FirstApp/Ch2_FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ch2_FirstApp
+//
+//  Created by 이해린 on 2021/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
